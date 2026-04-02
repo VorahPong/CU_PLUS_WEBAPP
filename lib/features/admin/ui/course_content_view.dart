8ee1734d5692f675977d93d92b0396c0a0701746
solistis <solistis01@gmail.com>
2026-04-02 15:23:56 -0500

    added course content buttons

--- lib/features/admin/ui/course_content_view.dart
+++ lib/features/admin/ui/course_content_view.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
 
-class CourseContentView extends StatelessWidget {
+class CourseContentView extends StatefulWidget {
   const CourseContentView({
     super.key,
     required this.email,
   });
 
   final String email;
+
+  @override
+  State<CourseContentView> createState() => _CourseContentViewState();
+}
+
+class _CourseContentViewState extends State<CourseContentView> {
+  bool isEditMode = true;
 
   @override
   Widget build(BuildContext context) {
@@ -22,16 +29,63 @@ class CourseContentView extends StatelessWidget {
               fontWeight: FontWeight.bold,
             ),
           ),
-          const SizedBox(height: 16),
-
-          Text(
-            "Logged in as: $email",
-            style: const TextStyle(fontSize: 16),
-          ),
 
           const SizedBox(height: 24),
 
-          // Example placeholder content
+          // Top row: toggle on left, create folder button on right
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 12,
+                  vertical: 4,
+                ),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey.shade400),
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.white,
+                ),
+                child: Row(
+                  children: [
+                    const Text(
+                      "Toggle Edit Mode",
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    const SizedBox(width: 12),
+                    Switch(
+                      value: isEditMode,
+                      onChanged: (value) {
+                        setState(() {
+                          isEditMode = value;
+                        });
+                      },
+                    ),
+                  ],
+                ),
+              ),
+
+              OutlinedButton.icon(
+                onPressed: () {
+                  // functionality will be added later
+                },
+                icon: const Icon(Icons.add_circle_outline),
+                label: const Text("Create new folder"),
+                style: OutlinedButton.styleFrom(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 18,
+                    vertical: 18,
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              ),
+            ],
+          ),
+
+          const SizedBox(height: 32),
+
           Expanded(
             child: Container(
               width: double.infinity,
@@ -41,8 +95,12 @@ class CourseContentView extends StatelessWidget {
               ),
               child: const Center(
                 child: Text(
-                  "Course content will go here",
-                  style: TextStyle(fontSize: 18),
+                  "Empty",
+                  style: TextStyle(
+                    fontSize: 40,
+                    fontStyle: FontStyle.italic,
+                    color: Colors.grey,
+                  ),
                 ),
               ),
             ),
