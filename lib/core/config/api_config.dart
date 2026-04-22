f1e8d92a10ebc20e48bbf97297b63c3a774ec721
VorahPong <vorahpongmean@gmail.com>
2026-04-21 23:54:50 -0500

    Update deploy.yml for backend url

--- lib/core/config/api_config.dart
+++ lib/core/config/api_config.dart
@@ -1,3 +1,6 @@
 class ApiConfig {
-  static const String baseUrl = "http://localhost:4000";
+  static const String baseUrl = String.fromEnvironment(
+    'API_BASE_URL',
+    defaultValue: 'http://localhost:4000',
+  );
 }
